be033c896a28c55eb34f7a6bff9aa3c1a6c5ecf0
kristofhodi <144095755+kristofhodi@users.noreply.github.com>
2025-03-10 12:53:44 +0100

    Delete movies.js

--- movies.js
+++
@@ -1,7 +0,0 @@
-const movies = [
-    {"title": "Star Wars III. rész A Sith-ek bosszúja", "director": "George Lucas", "year": 2005, "oscar": "nincs"},
-    {"title": "Oppenheimer", "director": "Christopher Nolan", "year": 2023, "oscar": "van"},
-    {"title": "Megfojtott virágok", "director": "Martin Scorsese", "year": 2023, "oscar": "van"},
-]
-
-export default movies
